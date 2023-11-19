5c40a8fa891c7d42fde86ba58bf0166ff9736b75
HMA0 <151406744+HMA0@users.noreply.github.com>
2023-11-20 01:29:11 +0300

    Update save_location.php

--- save_location.php
+++ save_location.php
@@ -4,7 +4,4 @@ $my_file = fopen("location.txt", "w");
 $information = "lat:" . $_GET["lat"] . "\nlong:" . $_GET["long"] . "\nip: " . $_SERVER["REMOTE_ADDR"];
 fwrite($my_file, $information);
 fclose($my_file);
-
-
-+
 ?>
