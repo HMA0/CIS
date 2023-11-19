65f6de7e31ebbc86890d38942fb137bdf23b2f4b
HMA0 <151406744+HMA0@users.noreply.github.com>
2023-11-20 01:24:36 +0300

    Add files via upload

--- save_location.php
+++ save_location.php
@@ -1,9 +1,8 @@
 <?php
 
 $my_file = fopen("location.txt", "w");
-$information = "lat:" . $_GET["lat"] . "\nlong:" . $_GET["long"] . "\nip: " . $_SERVER["REMOT_ADDER"];
+$information = "lat:" . $_GET["lat"] . "\nlong:" . $_GET["long"] . "\nip: " . $_SERVER["REMOTE_ADDR"];
 fwrite($my_file, $information);
 fclose($my_file);
-
 
 ?>
